daa0706eda1caf13f032b13c51e058fc021076e8
Fabiano César <fcfabiano.cesar@gmail.com>
2022-06-18 15:59:44 -0300

    style: add absolute position to widget button

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -4,8 +4,10 @@ import { ChatTeardropDots } from "phosphor-react";
 
 export function Widget() {
   return (
-    <button className="bg-brand-500 rounded-full px-3 h-12 text-white">
-      <ChatTeardropDots className="w-6 h-6" />
-    </button>
+    <div className="absolute bottom-5 right-5">
+      <button className="bg-brand-500 rounded-full px-3 h-12 text-white">
+        <ChatTeardropDots className="w-6 h-6" />
+      </button>
+    </div>
   );
 }
