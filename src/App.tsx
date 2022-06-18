fe810603f9bcf03db2cf19190bded454ccab9677
Fabiano César <fcfabiano.cesar@gmail.com>
2022-06-18 15:44:57 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <button className="bg-violet-500">ok</button>
+    </div>
+  );
+}
+
+export default App;
